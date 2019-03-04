4839c27b5644147fdcb4bf9c16e1f20a71db8052
Tyler Williams <tw1235@msstate.edu>
2019-03-03 22:09:05 -0500

    trying new approach: mdbootstrap; supposedly works with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+// Let * be the statement "the preceding is necessary for Angular Material"
+// There are other dependencies located in material.module.ts and styles.css.
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; //*
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -6,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FrontPageComponent } from './components/front-page/front-page.component';
 import { CompanySignupComponent } from './components/company-signup/company-signup.component';
@@ -22,6 +26,8 @@ import { CompanyJobListingsComponent } from './components/company-job-listings/c
 import { AuthService } from './services/auth.service';
 import { StudentService } from './services/student.service';
 import { CompanyService } from './services/company.service';
+import { MaterialModule } from './material.module'; //*
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -42,12 +48,15 @@ import { CompanyService } from './services/company.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+   // MaterialModule, //*
     ],
-  providers: [AuthService, StudentService, CompanyService],
-  bootstrap: [AppComponent]
+  providers: [AuthService, StudentService, CompanyService], //, MaterialModule], //*
+  bootstrap: [AppComponent],
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA] //*
 })
 export class AppModule { }
--- src/app/services/new-job-listing.service.ts
+++ src/app/services/new-job-listing.service.ts
@@ -1,18 +1,116 @@
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
+import { Component } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material';
 import { Injectable } from '@angular/core';
-import { Tags } from './../models/Tags.model';
+import { Tag } from '../models/tag/tag';
+
+
+
+@Component({
+    selector: 'new-job-listing',
+    templateUrl: './new-job-listing.component.html',
+    styleUrls: ['./new-job-listing.component.css']
+})
 
 @Injectable()
 export class NewJobListingService {
+    visible = true;
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA, SPACE];
 
-    jobTags: Tags = { 
-        tagList: []
-    };
+
+    tags: Tag[] = [];
+
+    add(event: MatChipInputEvent): void {
+        const input = event.input;
+        const value = event.value;
+
+        // Add our tag
+        if ((value || '').trim()) {
+            this.tags.push({ tagName: value.trim() });
+        }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    remove(tag: Tag): void {
+        const index = this.tags.indexOf(tag);
+
+        if (index >= 0) {
+            this.tags.splice(index, 1);
+        }
+    }
+}
+// import {COMMA, ENTER, SPACE} from '@angular/cdk/keycodes';
+// import {Component} from '@angular/core';
+// import {MatChipInputEvent} from '@angular/material';
+
+// export interface Fruit {
+//   name: string;
+// }
+
+/**
+ * @title Chips with input
+ */
+// @Component({
+//   selector: 'chips-input-example',
+//   templateUrl: 'chips-input-example.html',
+//   styleUrls: ['chips-input-example.css'],
+// })
+// export class ChipsInputExample {
+//   visible = true;
+//   selectable = true;
+//   removable = true;
+//   addOnBlur = true;
+//   readonly separatorKeysCodes: number[] = [ENTER, COMMA, SPACE];
+//   fruits: Fruit[] = [
+//     {name: 'Lemon'},
+//     {name: 'Lime'},
+//     {name: 'Apple'},
+//   ];
+
+//   add(event: MatChipInputEvent): void {
+//     const input = event.input;
+//     const value = event.value;
+
+//     // Add our fruit
+//     if ((value || '').trim()) {
+//       this.fruits.push({name: value.trim()});
+//     }
+
+//     // Reset the input value
+//     if (input) {
+//       input.value = '';
+//     }
+//   }
+
+//   remove(fruit: Fruit): void {
+//     const index = this.fruits.indexOf(fruit);
+
+//     if (index >= 0) {
+//       this.fruits.splice(index, 1);
+//     }
+//   }
+// }
+
+
+    // addTags(inputTags: [string, string, string, string, string]) {
+    //     let i: number;
+    //     for (i = 0; i < 5; i++) {
+    //         this.jobTags.tagList[i] = inputTags[i];
+    //     }
+    // }
+
+
+
+
 
     //tagsList: Array<string> = [];
-
-    addTag(inputTags: string) {
-        this.jobTags.tagList.push(inputTags);
-    }
 
     // updateTags(): Tags {
     //     this.jobTags = {
@@ -21,13 +119,11 @@ export class NewJobListingService {
     //     return this.jobTags;
     // }
 
-}
+//}
 
 
 // let service = new NewJobListingService;
 
-// service.addTag("one");
-// service.addTag("two");
-// service.addTag("three");
+// service.addTags(["a", "b", "c", "d", "e"]);
 
 // console.log(service.jobTags);
